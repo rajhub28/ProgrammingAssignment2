aebcf16076f31e268a91d18e148621824fd7e3ba
rajhub28 <raj@cs.gsu.edu>
2014-10-26 13:18:30 -0400

    My assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+##
+## This function conatructs a Cache Matrix object
+## with the ability to save the inverse of the
+## matrix and recalled when needed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(m) {
+    mat <<- m
+    inv <<- NULL
+  }
+  get <- function() mat
+  setinverse <- function(i) inv <<- i
+  getinverse <- function() inv
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+##
+## This function computes the inverse of a matrix, assuming it has one
+## It first checks if the inverse is already available and if so, it
+## simply returns the inverse rather than calculate it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setinverse(inv)
+  inv
 }
---
+++ driver.R
@@ -0,0 +1,6 @@
+m1 <- matrix(c(4.0,7.0,2.0,6.0),nrow=2,ncol=2,byrow=TRUE)
+cm1 <- makeCacheMatrix()
+cm1$set(m1)
+print(cacheSolve(cm1))
+print(cacheSolve(cm1))
+print(cacheSolve(cm1))
